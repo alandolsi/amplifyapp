815c2388cafd961392af43c2ddb328e6fd561224
Abdellatif Landolsi <abdellatif@landolsi.de>
2021-09-06 18:27:28 +0200

    make some changes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App Deployed Successfully on AWS!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.landolsi.de"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Webdesign and Development by Landolsi Webdesign.
         </a>
       </header>
     </div>
